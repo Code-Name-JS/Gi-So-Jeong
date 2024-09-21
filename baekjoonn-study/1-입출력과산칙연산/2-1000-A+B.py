e509fddf4f14bc65e272b858a51cd3639d15b375
Gi-So-Jeong <sgi716536@gmail.com>
2024-09-21 19:56:10 +0900

    Commit changes in submodule

--- baekjoonn-study/1-입출력과산칙연산/2-1000-A+B.py
+++
@@ -1,6 +0,0 @@
-A, B = map(int, input().split())
-print(A + B)
-
-A = int(input())
-B = int(input())
-print(A + B)
--- baekjoonn-study/1-입출력과산칙연산/7-10926-??!.py
+++
@@ -1,5 +0,0 @@
-i = input().strip()
-if i.islower() and i.isalpha() and len(i) <= 50:
-    print(i + "??!")
-else:
-    print(i + "??!")
